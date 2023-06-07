819178820d0316ba12d28b5dc79acdcbd764f8b1
prodipdev <prodipink@gmail.com>
2023-06-07 15:16:02 +0600

    Create Footer

--- src/Layouts/Main.jsx
+++ src/Layouts/Main.jsx
@@ -1,11 +1,15 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../pages/Shared/Navbar";
+import Footer from "../pages/Shared/Footer";
 
 const Main = () => {
   return (
     <div>
       <Navbar />
-      <Outlet />
+      <div className="min-h-[calc(100vh-427.68px)] bg-gray-100">
+        <Outlet></Outlet>
+      </div>
+      <Footer />
     </div>
   );
 };
